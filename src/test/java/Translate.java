0401b420b87032cf6060a41a6ba5ac9333696e94
Beatoriche-sama <77235839+Beatoriche-sama@users.noreply.github.com>
2021-01-18 11:24:15 +0300

    Update Translate.java
    quick translate

--- src/test/java/Translate.java
+++ src/test/java/Translate.java
@@ -1,52 +1,42 @@
-import com.google.common.net.UrlEscapers;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Translate {
     //"auto" - from
     //to - выбор языка внутри трея
     //en - english, ja - japanese, ru - russian
-    /*
-     String url = "https://www.deepl.com/translator#"
-                        + from.getLanguageCode()
-                        + "/"
-                        + to.getLanguageCode()
-                        + "/"
-                        +  UrlEscapers.urlFragmentEscaper().escape(text);
-     */
-    public String urlEncode(String baseURL, String query) {
-        String encodeStr = UrlEscapers.urlFragmentEscaper().escape(query);
-        //String encodeStr = URLEncoder.encode(query, StandardCharsets.UTF_8.name());
-        return baseURL + encodeStr;
+
+
+    private static String regex(String text){
+        Pattern p = Pattern.compile("\"([^\"]*)\"");
+        Matcher m = p.matcher(text);
+        String regexResult = null;
+        if (m.find()) {
+            regexResult = m.group(1);
+        }
+        return regexResult;
     }
 
-    public String translate(String textToTranslate) throws InterruptedException, UnsupportedEncodingException {
-        System.setProperty("webdriver.chrome.whitelistedIps", "");
-        System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
-        /*
-        JBrowserDriver driver = new JBrowserDriver(Settings.builder().
-                timezone(Timezone.EUROPE_MOSCOW).build());
-        */
+    public String translate(String textToTranslate) throws IOException {
+        textToTranslate = URLEncoder.encode(textToTranslate, StandardCharsets.UTF_8);
+        URL url = new URL("http://translate.googleapis.com/translate_a/single?client=gtx&sl=" + "en" + "&tl="
+                + "ru" + "&dt=t&q=" + textToTranslate + "&ie=UTF-8&oe=UTF-8");
 
-        DesiredCapabilities desireCaps = new DesiredCapabilities();
-        desireCaps.setJavascriptEnabled(true);
-        desireCaps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-                "D:/libs/phantomjs/bin/phantomjs.exe");
-        WebDriver driver = new PhantomJSDriver(desireCaps);
+        URLConnection uc = url.openConnection();
+        uc.setRequestProperty("User-Agent", "Mozilla/5.0");
 
-        //WebDriver driver = new ChromeDriver(options);
-        String url = urlEncode("https://www.deepl.com/translator#en/ru/", textToTranslate);
-        driver.get(url);
-        Thread.sleep(10000);
-        WebElement webElement = driver.findElement(By.className("lmt__translations_as_text__text_btn"));
-        String translatedText = webElement.getAttribute("textContent");
-        driver.quit();
-        return translatedText;
+        BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream(), StandardCharsets.UTF_8));
+        String inputLine;
+        StringBuilder result = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            result.append(inputLine);
+        }
+        in.close();
+        return regex(String.valueOf(result));
     }
 }
